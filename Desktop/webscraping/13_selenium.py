e1b525fda79f47110c4ce613495e407ced114f00
Leeuichan <109333351+Leeuichan@users.noreply.github.com>
2023-02-21 20:36:18 +0900

    Update 13_selenium.py

--- Desktop/webscraping/13_selenium.py
+++ Desktop/webscraping/13_selenium.py
@@ -4,15 +4,13 @@ from selenium.webdriver.common.by import By
 chrome_option = webdriver.ChromeOptions()
 chrome_option.add_experimental_option("detach", True)
 
-
- 
-#browser = webdriver.Chrome()
-#browser.get("http://naver.com")
-
+#Chrome driver 이용
 def open_chrome(url):
     driver = webdriver.Chrome(chrome_options=chrome_option)
     driver.get(url)
-    
+ #네이버 url지정
 open_chrome("http://naver.com")
+# 네이버 로그인 element 지정
 elem = open_chrome.driver.find_element(By.CLASS_NAME, 'link_login')
+#login click
 elem.click()
