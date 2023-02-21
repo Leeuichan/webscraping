2aad2908debc827a4986ad76d34bfdf99d0cbf29
Leeuichan <109333351+Leeuichan@users.noreply.github.com>
2023-02-21 20:42:34 +0900

    Update 8_grow.py

--- Desktop/webscraping/8_grow.py
+++ Desktop/webscraping/8_grow.py
@@ -5,13 +5,15 @@ from bs4 import BeautifulSoup
 
 url = "https://comic.naver.com/webtoon/list?titleId=776255"
 
+#class title 구분하여 가져오기 
+grow = soup.find_all("td",attrs={"class":"title"})
+#element 'a'에 해당하는 index[0] 웹툰 title 가져오기
+title = grow[0].a.get_text()
+#web 주소별 페이지 ex) 헤당 웹툰 page 가져오기
+link = grow[0].a["href"]
 
-#grow = soup.find_all("td",attrs={"class":"title"})
-
-#title = grow[0].a.get_text()
-#link = grow[0].a["href"]
-#print(title)
-#print("https://comic.naver.com" + link)
+print(title)
+print("https://comic.naver.com" + link)
 
 #만화 제목 링크 가져오기
 #for cartoon in grow:
