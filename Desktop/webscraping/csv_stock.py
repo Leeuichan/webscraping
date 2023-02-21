be09d5129ba473111521b9a104663a1a6212358d
Leeuichan <109333351+Leeuichan@users.noreply.github.com>
2023-02-21 20:46:14 +0900

    Update csv_stock.py

--- Desktop/webscraping/csv_stock.py
+++ Desktop/webscraping/csv_stock.py
@@ -2,15 +2,19 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
+#webscraping으로 csv만들기
+
 url =  "https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page="
 
 filename = "시가총액 .csv"
 f = open(filename, "w", encoding="utf-8-sig", newline="")
 writer = csv.writer(f)
 
+#테이블 데이터 title 설정
 title = "N	종목명	현재가	전일비	등락률	액면가	시가총액	상장주식수	외국인비율	거래량	PER	ROE".split("\t")
 
 writer.writerow(title)
+
 
 for page in range(1,5):
     res = requests.get(url + str(page))
